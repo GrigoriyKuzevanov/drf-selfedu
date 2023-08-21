d0bf7e2e58b8485e5273c4c6d7564491f2a0bd62
Григорий Кузеванов <grkuzevanov@gmail.com>
2023-08-21 15:27:22 +0500

    Create app women

--- drfsite/drfsite/settings.py
+++ drfsite/drfsite/settings.py
@@ -41,6 +41,8 @@ INSTALLED_APPS = [
     'django.contrib.sessions',
     'django.contrib.messages',
     'django.contrib.staticfiles',
+
+    'women.apps.WomenConfig',
 ]
 
 MIDDLEWARE = [
@@ -111,7 +113,7 @@ AUTH_PASSWORD_VALIDATORS = [
 # Internationalization
 # https://docs.djangoproject.com/en/4.2/topics/i18n/
 
-LANGUAGE_CODE = 'en-us'
+LANGUAGE_CODE = 'ru'
 
 TIME_ZONE = 'UTC'
 
